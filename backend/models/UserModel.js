1800b73da91fb90f29b3f33291ff3480690722ee
Ankit Baidsen <145530612+neetance@users.noreply.github.com>
2024-07-21 12:08:08 +0530

    Add files via upload

---
+++ backend/models/UserModel.js
@@ -0,0 +1,16 @@
+import { Schema as _Schema, model } from 'mongoose'
+const Schema = _Schema
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    coins: {type: Number, default: 0},
+    city: {type: String, default: null},
+    state: {type: String, default: null},
+    familyMembers: {type: Number, default: 0},
+    cityAvg: {type: Number, default: null}
+})
+
+const User = model('User', userSchema)
+export default User
