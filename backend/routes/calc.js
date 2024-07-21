048463ff4b03f35965abb65ad552d2ea3cab89da
Ankit Baidsen <145530612+neetance@users.noreply.github.com>
2024-07-21 12:59:27 +0530

    Update calc.js

--- backend/routes/calc.js
+++ backend/routes/calc.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import { createEmission, getEmission, getALlEmissions, getLast12Emissions } from '../controllers/EmissionController.js';
-import {authenticateUser} from '../controllers/usercontroller.js'
+import {authenticateUser} from '../controllers/UserController.js'
 
 router.post('/', authenticateUser, createEmission)
 router.post('/getEmission', authenticateUser, getEmission)
