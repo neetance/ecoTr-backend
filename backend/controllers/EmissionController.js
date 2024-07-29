aa047d9c472672c587258304277d9177f8e5f949
Ankit Baidsen <145530612+neetance@users.noreply.github.com>
2024-07-29 21:56:06 +0530

    Update EmissionController.js

--- backend/controllers/EmissionController.js
+++ backend/controllers/EmissionController.js
@@ -4,6 +4,7 @@ export async function createEmission(req, res) {
     try {
         const {month, year, area, electricity, gas, gasusage, wood, priv, waste, meal, meals, renewable, renewunit} = req.body
         const email = req.user.email
+        console.log(email)
 
         const checkEmission = await Emission.findOne({
             user: email,
